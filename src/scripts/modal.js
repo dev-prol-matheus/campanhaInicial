47aae3e42079d340560a66b857ec878fb957f3ba
Kevison Pascoal Mendes <kevison.pascoal@gmail.com>
2022-09-13 13:26:30 -0300

    updated responsive:package:

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -2,11 +2,9 @@ console.log("Hello World!");
 const modalContainer = document.getElementById("modal-container");
 const closeModalBtn = document.getElementById("close-modal-btn");
 
-/*
 setTimeout(function(){
     modalContainer.classList.add("show-modal");
-}, 100000);
-*/
+}, 1000);
 
 closeModalBtn.addEventListener("click", () => {
     modalContainer.classList.remove("show-modal");
