817f6ab30ac68607564874274bb0e41b3f56391b
Marcos Aurelio <marcosaurelio@proleduca.com.br>
2022-12-02 12:22:18 -0300

    configuration(components,cookies):minify code:hammer:

--- view/assets/scripts/modal.js
+++ view/assets/scripts/modal.js
@@ -1,11 +1,1 @@
-// console.log("Hello World!");
-const modalContainer = document.getElementById("modal-container");
-const closeModalBtn = document.getElementById("close-modal-btn");
-
-setTimeout(function(){
-    modalContainer.classList.add("show-modal");
-}, 20000);
-
-closeModalBtn.addEventListener("click", () => {
-    modalContainer.classList.remove("show-modal");
-});
+const modalContainer = document.getElementById("modal-container"); const closeModalBtn = document.getElementById("close-modal-btn"); setTimeout(function(){ modalContainer.classList.add("show-modal"); }, 20000); closeModalBtn.addEventListener("click", () => { modalContainer.classList.remove("show-modal"); });
