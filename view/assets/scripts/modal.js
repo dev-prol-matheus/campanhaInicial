bb2cc890ebaea2af34290673ee03b625891c3945
Marcos Aurelio <marcosaurelio@proleduca.com.br>
2022-12-15 11:58:21 -0300

    feature(scripts):add methods in buttons, comment modal code :card_index:

--- view/assets/scripts/modal.js
+++ view/assets/scripts/modal.js
@@ -1,1 +1,8 @@
-const modalContainer = document.getElementById("modal-container"); const closeModalBtn = document.getElementById("close-modal-btn"); setTimeout(function(){ modalContainer.classList.add("show-modal"); }, 20000); closeModalBtn.addEventListener("click", () => { modalContainer.classList.remove("show-modal"); });
+// const modalContainer = document.getElementById("modal-container");
+// const closeModalBtn = document.getElementById("close-modal-btn");
+
+// setTimeout(function(){ modalContainer.classList.add("show-modal"); }, 20000);
+
+// closeModalBtn.addEventListener("click", () => {
+//   modalContainer.classList.remove("show-modal");
+// });
