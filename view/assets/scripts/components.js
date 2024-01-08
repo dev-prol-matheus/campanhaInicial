5bfb302e54f961953e0aac592bfab8661dacdd91
Matheus Eloim <matheuseloim@proleduca.com.br>
2024-01-08 17:47:12 -0300

    ajustes nos botões

--- view/assets/scripts/components.js
+++ view/assets/scripts/components.js
@@ -4,11 +4,11 @@ const modal = document.getElementById("modal");
 
 function createButtonComponents() {
   buttonComponent.forEach((button) => {
-    button.innerHTML = `<img id="whatsapp-img" src="./view/assets/image/icons/whatsapp_icon.webp"
+    button.innerHTML = `<a href="#container-form-whatsapp"><img id="whatsapp-img" src="./view/assets/image/icons/whatsapp_icon.webp"
       alt="Whatsapp"
       title="Entre com contato pelo Whatsapp">
       <h5>GARANTA AGORA SUA BOLSA <br> PELO WHATSAPP
-      </h5> `;
+      </h5></a>`;
     button.addEventListener('click',assignMethod);
   });
 }
