b6ecb7bb72dd69a09a4cefb4f6bddd0120d4c66b
Marcos Aurelio <marcosaurelio@proleduca.com.br>
2024-01-09 14:48:55 -0300

    change(index): refactor responsive :hammer:

--- view/assets/scripts/components.js
+++ view/assets/scripts/components.js
@@ -2,17 +2,30 @@ const buttonComponent = document.querySelectorAll('.button_component');
 const modalContainer = document.getElementById("modal-container");
 const modal = document.getElementById("modal");
 
-function createButtonComponents() {
-  buttonComponent.forEach((button) => {
-    button.innerHTML = `<img id="whatsapp-img" src="./view/assets/image/icons/whatsapp_icon.webp"
-      alt="Whatsapp"
-      title="Entre com contato pelo Whatsapp">
-      <h5>GARANTA AGORA SUA BOLSA <br> PELO WHATSAPP
-      </h5> `;
-    button.addEventListener('click',assignMethod);
-  });
+// function createButtonComponents() {
+//   buttonComponent.forEach((button) => {
+//     button.innerHTML = `<img id="whatsapp-img" src="./view/assets/image/icons/whatsapp_icon.webp"
+//       alt="Whatsapp"
+//       title="Entre com contato pelo Whatsapp">
+//       <h5>GARANTA AGORA SUA BOLSA <br> PELO WHATSAPP
+//       </h5> `;
+//     button.addEventListener('click',assignMethod);
+//   });
+// }
+
+function createButton() {
+  const formLinks = document.querySelectorAll(".form_whatsapp_link");
+  formLinks.forEach(links => {
+    links.innerHTML = `
+      <img src="./view/assets/image/icons/whatsapp_icon.webp" alt="Whatsapp">
+      <button>
+        GARANTA AGORA SUA BOLSA <br> PELO WHATSAPP
+      </button>
+    `;
+  })
 }
-createButtonComponents();
+
+createButton();
 
 function assignMethod() {
   modalContainer.classList.remove("hidden");
